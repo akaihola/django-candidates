d13b357d229deb293163a9b49d3373fc171abbad
Antti Kaihola <akaihol+django@ambitone.com>
2009-10-23 14:35:07 +0300

    Extracted class-based views implementation into the separate classyviews app

--- candidates/templatetags/candidates_tags.py
+++ candidates/templatetags/candidates_tags.py
@@ -1,58 +1,16 @@
 from django import template
 register = template.Library()
 
-from django.core.urlresolvers import \
-    get_callable, get_mod_func, ViewDoesNotExist
-
-class WithViewContextNode(template.Node):
-    def __init__(self, view, nodelist):
-        self.view = view
-        self.nodelist = nodelist
-
-    def __repr__(self):
-        return "<WithViewContextNode>"
-
-    def render(self, context):
-        view = self.view.resolve(context)
-        if not callable(view):
-            try:
-                view = get_callable(view)
-            except ImportError, e:
-                mod_name, _ = get_mod_func(view)
-                raise ViewDoesNotExist(
-                    "Could not import %s. Error was: %s" % (mod_name, str(e)))
-            except AttributeError, e:
-                mod_name, func_name = get_mod_func(view)
-                raise ViewDoesNotExist("Tried %s in module %s. Error was: %s" %
-                                       (func_name, mod_name, str(e)))
-        response = view(context['request'], _render=False)
-        context.push()
-        context.update(response.context)
-        output = self.nodelist.render(context)
-        context.pop()
-        return output
-
-@register.tag
-def with_view_context(parser, token):
+@register.inclusion_tag('candidates/_field.html')
+def application_form_field(field, alt_label=''):
     """
-    Calls a class-based view, but instead of rendering a template, only
-    requests the context from the view and uses it inside of this block for
-    caching and easy access.
+    Print HTML for a field.  Optionally, a label can be supplied that overrides
+    the default label generated for the form.
 
-    For example::
-
-        {% with_view_context "myapp.views.MyView" %}
-          {% include "a-template-using-MyView-context.html" %}
-        {% endwith %}
+    Example:
+    {% display_field form.my_field "My New Label" %}
     """
-    bits = list(token.split_contents())
-    if len(bits) != 2:
-        raise TemplateSyntaxError("%r expects view as its only argument" %
-                                  bits[0])
-    view = parser.compile_filter(bits[1])
-    if isinstance(view, basestring) and not view:
-        raise TemplateSyntaxError(
-                "%r tag doesn't permit an empty view name" % bits[0])
-    nodelist = parser.parse(('end_with_view_context',))
-    parser.delete_first_token()
-    return WithViewContextNode(view, nodelist)
+
+    if alt_label:
+        field.label = alt_label
+    return {'field': field}
