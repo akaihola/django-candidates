f044baff395ef958d9add329bb115b6406b9aca1
Antti Kaihola <akaihol+ledgersmb@ambitone.com>
2010-01-15 14:53:20 +0200

    Syncdb and runserver now work for the example project

--- example/settings.py
+++ example/settings.py
@@ -1,7 +1,19 @@
+from os.path import join, dirname
+PROJECT_ROOT = dirname(__file__)
+
 DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = ':memory:'
+DATABASE_NAME = join(PROJECT_ROOT, 'example.sqlite')
 DATABASE_SUPPORTS_TRANSACTIONS = False
 
-INSTALLED_APPS = ('candidates',
-                  'django_nose',)
+ROOT_URLCONF = 'example.urls'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+INSTALLED_APPS = ('django.contrib.auth',
+                  'django.contrib.contenttypes',
+                  'django.contrib.sessions',
+                  'django_nose',
+                  'candidates',
+                  'candidates_test_app',)
 TEST_RUNNER = 'django_nose.run_tests'
