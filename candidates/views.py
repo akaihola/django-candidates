883c06134b5af4e7423b8598d74b2b9bb9b5740e
Antti Kaihola <akaihol+ledgersmb@ambitone.com>
2010-01-15 14:51:52 +0200

    Added form creation and request handling methods to the EditApplication view

--- candidates/views.py
+++ candidates/views.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date
+
 from django.db import transaction
+from django.conf import settings
 from django.utils.cache import add_never_cache_headers
+from django.contrib.auth import login, logout, authenticate
+from django.forms.formsets import all_valid
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 from classyviews import ClassyView
 
+from candidates.forms import UserForm
+
 class MetaBase:
+    @classmethod
+    def current_round_name(cls):
+        raise NotImplementedError(
+            '%s must define the current_round_name() method' % cls.__name__)
+
     @staticmethod
     def get_deadline():
         raise NotImplementedError(
@@ -41,6 +55,166 @@ class EditApplicationBase(ApplicationViewBase):
     def POST(self, request, username=''):
         return self.handle_request(request,
                                    request.POST, request.FILES, username)
+
+    @classmethod
+    def handle_request(cls, request, data, files, username):
+        """Handle HTTP requests for creating and editing applications
+
+        Handle both GET and POST requests to the public and private
+        interfaces.
+
+        Return value can be:
+        * the context dictionary to be used when rendering the
+          template
+        * a :class:`HttpResponseRedirect` object
+
+        Public interface
+        ================
+
+        * Can't redirect due to embedding
+        * Anonymous users, logged in users and the secretary always see a blank
+          form
+        * When a valid form is saved:
+          * secretary sees a link to editing the new application through the
+            private interface
+          * anonymous and other users are logged in as the user of the new
+            application
+
+        Private interface
+        =================
+
+        * Only accessible to the secretary
+        * Only for modifying existing applications
+        """
+        if data and 'clear' in data:
+            logout(request)
+            data = None
+            files = None
+            username = ''
+
+        public_interface = username == ''
+        secretary = request.user.has_perm('%s.%s' % (
+                cls.meta.model._meta.app_label,
+                cls.meta.model._meta.get_change_permission()))
+
+        if not secretary:
+            if not public_interface:
+                return cls.redirect_to_login('')
+            elif date.today() > cls.meta.get_deadline():
+                return {'past_deadline': True}
+
+        user = None
+        app = None
+
+        if public_interface:
+            if not secretary and request.user.is_authenticated():
+                # Logged in user using the public interface: editing an
+                # existing application
+                user = request.user
+            # Else the secretary or an anonymous user is using the public
+            # interface to create a new application
+        else:
+            # Secretary is using the private interface to edit an existing
+            # application
+            user = User.objects.get(username=username)
+
+        saved = False
+        if user:
+            try:
+                app = cls.meta.model.objects.get(
+                    user=user,
+                    round_name=cls.meta.current_round_name())
+                saved = True
+            except cls.meta.model.DoesNotExist:
+                logout(request)
+
+        if app is None:
+            app = cls.meta.model()
+            if user is not None:
+                app.user = user
+
+        forms = cls.create_forms(data, files, user, app)
+        all_forms_valid = all_valid(forms)
+        if all_forms_valid:
+            # The application is valid and should be saved.
+            user = forms[0].save(commit=False)
+            username, application = cls.save(
+                user, *forms[1:], **{'is_secretary': secretary})
+            saved = True
+            if application.send_confirmation_email:
+                user, password = cls.assign_password(username)
+                cls.send_confirmation_email(application, password)
+                user = authenticate(username=username, password=password)
+            else:
+                user.backend = settings.AUTHENTICATION_BACKENDS[0]
+            if secretary:
+                # If the secretary saved a new valid application, show a link
+                # for editing it in the private interface. We can't do a HTTP
+                # redirect since the public interface is embedded on a CMS
+                # page.
+                return {'link_to_private': reverse(
+                        'edit-application', kwargs={'username': username})}
+            else:
+                # A visitor saved a valid application. Log in as the user of
+                # the application.
+                login(request, user)
+            forms = cls.create_forms(None, None, user, app)
+        should_confirm = False
+        if user:
+            app = user.applications.get(
+                round_name=cls.meta.current_round_name())
+            should_confirm = \
+                not app.send_confirmation_email and not app.confirmed
+        return dict(
+            forms=forms,
+            user_form=forms[0],
+            application_form=forms[1],
+            saved=saved,
+            has_errors=data is not None and not all_forms_valid,
+            should_confirm=should_confirm,
+            deadline=cls.meta.get_deadline())
+
+    @classmethod
+    def create_forms(cls, data, files, user, appl):
+        """
+        ``user``: an existing user (not secretary) or None
+
+        Inherited classes can extend this list of forms.  The first
+        two items must always be the user form and the application
+        form.
+
+        Example:
+        forms = super(InheritedClassName, cls).create_forms()
+        forms.extend(...)
+        return forms
+        """
+        forms = [
+            cls.create_user_form(
+                data, instance=user, prefix='user'),
+            cls.create_application_form(
+                data, instance=appl, prefix='application')]
+        forms.extend(cls.create_extra_forms(data, files, user, appl))
+        return forms
+
+    @classmethod
+    def create_user_form(cls, data, instance, prefix):
+        return UserForm(data=data, instance=instance, prefix=prefix,
+                        current_round_name=cls.meta.current_round_name())
+
+    @classmethod
+    def create_application_form(cls, data, instance, prefix):
+        """Create an application form
+
+        Subclasses must override this class method to create their
+        project-specific application form.
+        """
+        raise NotImplementedError(
+            '%s must implement the create_application_form class method' %
+            cls.__name__)
+
+    @classmethod
+    def create_extra_forms(cls, data, files, user, appl):
+        return []
 
 class ConfirmApplicationBase(ApplicationViewBase):
     template_name = 'candidates/confirm_application.html'
